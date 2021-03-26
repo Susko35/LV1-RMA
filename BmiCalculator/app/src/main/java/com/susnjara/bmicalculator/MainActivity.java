0a2769aa41b3ec837c7a7ee2366907483f136117
Antonio Šušnjara <64197201+Susko35@users.noreply.github.com>
2021-03-26 11:20:30 +0100

    First commit
    Create project

---
+++ BmiCalculator/app/src/main/java/com/susnjara/bmicalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.susnjara.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
