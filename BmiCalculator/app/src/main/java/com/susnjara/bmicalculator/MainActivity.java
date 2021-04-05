3b088d467f22e5ed965cfca98d1c5675ffb83101
Antonio Šušnjara <64197201+Susko35@users.noreply.github.com>
2021-04-05 17:31:52 +0200

    Add color text, visibility image

--- BmiCalculator/app/src/main/java/com/susnjara/bmicalculator/MainActivity.java
+++ BmiCalculator/app/src/main/java/com/susnjara/bmicalculator/MainActivity.java
@@ -2,12 +2,16 @@ package com.susnjara.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button calculateButton;
     private TextView resultTextView;
     private TextView bmiTextView;
+    private ImageView bmiImage;
+    private TextView bmixTextView;
 
 
     @Override
@@ -37,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         calculateButton = findViewById(R.id.calculateButton);
         resultTextView = findViewById(R.id.resultTextView);
         bmiTextView = findViewById(R.id.bmiTextView);
+        bmiImage = findViewById(R.id.bmiImage);
+        bmixTextView = findViewById(R.id.bmixTextView);
 
         calculateButton.setOnClickListener(this);
     }
@@ -58,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             {
                 resultTextView.setText(String.format("%.2f", bmi));
                 checkBmiState(bmi);
+                bmiImage.setVisibility(View.VISIBLE);
             }
             else{
                 resultTextView.setText("Invalid values");
@@ -72,22 +81,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(bmi < 18.5)
         {
             bmiTextView.setText("Underweight");
+            bmixTextView.setTextColor(Color.rgb(83, 193, 237));
         }
         else if(bmi >= 18.5 && bmi < 24.9)
         {
             bmiTextView.setText("Healthy");
+            bmixTextView.setTextColor(Color.rgb(56, 242, 62));
         }
         else if(bmi >= 24.9 && bmi < 29.9)
         {
             bmiTextView.setText("Overweight");
+            bmixTextView.setTextColor(Color.rgb(241, 252, 76));
         }
         else if(bmi >= 29.9 && bmi < 34.9)
         {
             bmiTextView.setText("Obese");
+            bmixTextView.setTextColor(Color.rgb(255, 164, 36));
         }
         else
         {
             bmiTextView.setText(" Extremely obese");
+            bmixTextView.setTextColor(Color.rgb(255, 0, 21));
         }
 
     }
